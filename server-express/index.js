1ab6489278f427151762e61d6c8684d676fb5ffe
amitdervadiya <amitdervadiya@gmail.com>
2024-12-10 20:28:11 +0530

    crud-express

--- server-express/index.js
+++ server-express/index.js
@@ -1,39 +1,32 @@
-const { name } = require('ejs');
-const express = require('express');
+const express = require("express");
+let students = [];
+
 const port = 1008;
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.use(express.urlencoded());
 
-const students = [
-    {
-        'id': '1',
-        'name': 'amit',
-        'age': '18'
-    }, {
-        'id': '2',
-        'name': 'rahil',
-        'age': '18'
-    }, {
-        'id': '3',
-        'name': 'sanjay',
-        'age': '20'
-    },
-    {
-        'id': '4',
-        'name': 'sushil',
-        'age': '18'
-    }
-]
+app.get("/", (req, res) => {
+    res.render("index", { students });
+});
 
-app.get('/', (req, res) => {
-    res.render('index', { students });
+app.post("/adddata", (req, res) => {
+    req.body.id = String(Date.now())
+    students.push(req.body);
+    res.redirect("/");
+});
+
+app.get("/deletedata", (req, res) => {
+    const deleteid = String(req.query.id);
+    students = students.filter((el) => el.id !== deleteid);
+    res.redirect("/");
 });
 
 app.listen(port, (err) => {
     if (err) {
-        console.error("Error starting server:", err);
+        console.log("Server error:", err);
     } else {
-        console.log(`Server is running on http://localhost:${port}`);
+        console.log("Server started on port number " + port);
     }
 });
