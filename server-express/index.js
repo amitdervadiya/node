34c6d8622c96ee98ec2003a9169d9de415fbd291
amitdervadiya <amitdervadiya@gmail.com>
2024-12-11 20:21:29 +0530

    crud-express

--- server-express/index.js
+++ server-express/index.js
@@ -22,6 +22,29 @@ app.get("/deletedata", (req, res) => {
     students = students.filter((el) => el.id !== deleteid);
     res.redirect("/");
 });
+app.get('/editdata', (req, res) => {
+
+    console.log(req.query.id)
+    const updateid = req.query.id
+    const singledata = students.find((item) => item.id === updateid);
+    res.render('edit', { singledata })
+    console.log(singledata)
+
+})
+
+app.post("/updatedata", (req, res) => {
+    const updateid = req.body.id;
+
+    students.forEach((e) => {
+        if (e.id === updateid) {
+            e.name = req.body.name;
+            e.age = req.body.age;
+        }
+    });
+
+    res.redirect("/");
+});
+
 
 app.listen(port, (err) => {
     if (err) {
